0ed4678cecb9dbc66bce2bf15f2fe4bce958029f
komuro-ryuhei <k023g0040@g.neec.ac.jp>
2024-07-09 00:06:28 +0900

    04_00

--- main.cpp
+++ main.cpp
@@ -17,6 +17,33 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
+	struct Spring {
+		// アンカー
+		Vector3 ancor;
+		float naturalLength; // 自然長
+		float stiffness;     // 剛性、ばね定数k
+	};
+
+	Spring spring{};
+	spring.ancor = { 0.0f,0.0f,0.0f };
+	spring.naturalLength = 1.0f;
+	spring.stiffness = 100.0f;
+
+	struct Ball {
+		Vector3 position;    // ボールの位置
+		Vector3 velocity;    // ボールの速度
+		Vector3 cceleration; // ボールの加速度
+		float mass;          // ボールの質量
+		float radius;        // ボールの半径
+		unsigned int color;  // ボールの色
+	};
+
+	Ball ball{};
+	ball.position = { 1.2f,0.0f,0.0f };
+	ball.mass = 2.0f;
+	ball.radius = 0.05f;
+	ball.color = 0x0000FFFF;
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -29,6 +56,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+
+		Vector3 diff = ball.position - spring.ancor;
+		float length = Length(diff);
+		if (length != 0.0f) {
+
+			Vector3 direction = Normalize(diff);
+			Vector3 restPosition = spring.ancor + direction * spring.naturalLength;
+			Vector3 displacement = length * (ball.position - restPosition);
+		}
 
 		///
 		/// ↑更新処理ここまで
