96234c42ccc2f7986b746eb2704b8af26ccb1063
komuro-ryuhei <k023g0040@g.neec.ac.jp>
2024-07-09 00:24:55 +0900

    04_00

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	struct Ball {
 		Vector3 position;    // ボールの位置
 		Vector3 velocity;    // ボールの速度
-		Vector3 cceleration; // ボールの加速度
+		Vector3 aceleration; // ボールの加速度
 		float mass;          // ボールの質量
 		float radius;        // ボールの半径
 		unsigned int color;  // ボールの色
@@ -43,6 +43,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ball.mass = 2.0f;
 	ball.radius = 0.05f;
 	ball.color = 0x0000FFFF;
+
+	Sphere sphere;
+	sphere.center = ball.position;
+	sphere.radius = 0.1f;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -57,14 +61,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		CaluculationMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, { 0.0f,0.0f,0.0f });
+
+		float deltaTime = 1.0f / 60.0f;
+
 		Vector3 diff = ball.position - spring.ancor;
 		float length = Length(diff);
 		if (length != 0.0f) {
 
 			Vector3 direction = Normalize(diff);
 			Vector3 restPosition = spring.ancor + direction * spring.naturalLength;
-			Vector3 displacement = length * (ball.position - restPosition);
+			Vector3 displacement = (ball.position - restPosition) * length;
+			Vector3 restoringForce = displacement * -spring.stiffness;
+			Vector3 force = restoringForce;
+			ball.aceleration = force / ball.mass;
 		}
+
+		// 加速度も速度もどちらも秒を基準とした値
+		// それが、 1 / 60 秒間(deltaTime)適応したと考える
+		ball.velocity += ball.aceleration * deltaTime;
+		ball.position += ball.velocity * deltaTime;
+
+
+
+
 
 		///
 		/// ↑更新処理ここまで
@@ -73,6 +93,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		DrawGrid(worldViewProjectionMatrix, worldViewportMatrix);
+
+		DrawSphere(sphere, worldViewProjectionMatrix, worldViewportMatrix, ball.color);
+
+		Novice::DrawLine(int(spring.ancor.x), int(spring.ancor.y), int(sphere.center.x), int(sphere.center.y), 0xFFFFFFFF);
 
 		///
 		/// ↑描画処理ここまで
