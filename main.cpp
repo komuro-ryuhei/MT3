05053c98698f8a019116c77e60583ddebbf5dd56
komuro-ryuhei <k023g0040@g.neec.ac.jp>
2024-07-17 11:56:17 +0900

    04_02

--- main.cpp
+++ main.cpp
@@ -17,6 +17,34 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
+	struct Pendulum {
+
+		Vector3 anchor; // アンカーポイント
+		float length;   // 紐の長さ
+		float angle;    // 現在の角度
+		float angularVelocity;     // 角速度ω 
+		float angularAcceleration; // 角加速度
+	};
+
+	Pendulum pendulum;
+	pendulum.anchor = { 0.0f,1.0f,0.0f };
+	pendulum.length = 0.8f;
+	pendulum.angle = 0.7f;
+	pendulum.angularVelocity = 0.0f;
+	pendulum.angularAcceleration = 0.0f;
+
+
+	Sphere sphere;
+	sphere.center.x = pendulum.anchor.x + std::sin(pendulum.angle) * pendulum.length;
+	sphere.center.y = pendulum.anchor.y - std::cos(pendulum.angle) * pendulum.length;
+	sphere.center.z = pendulum.anchor.z;
+	sphere.radius = 0.05f;
+
+
+	 float deltaTime = 1.0f / 60.0f; // デルタタイム
+
+	 bool isMove = false;
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -30,6 +58,36 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		CaluculationMatrix({ 1.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f });
+
+		Matrix4x4 anchorWorldMatirx = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f });
+		Matrix4x4 sphereWorldMatirx = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f });
+
+		Matrix4x4 anchorWvpMatrix = Multiply(anchorWorldMatirx, Multiply(worldViewMatrix, projectionMatrix));
+		Matrix4x4 sphereWvpMatrix = Multiply(sphereWorldMatirx, Multiply(worldViewMatrix, projectionMatrix));
+
+
+		Matrix4x4 anchorLineWorldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, Vector3(anchorWorldMatirx.m[3][0], anchorWorldMatirx.m[3][1], anchorWorldMatirx.m[3][2]));
+		Matrix4x4 sphereLineWorldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, Vector3(sphereWorldMatirx.m[3][0], sphereWorldMatirx.m[3][1], sphereWorldMatirx.m[3][2]));
+
+		Matrix4x4 anchorLineWvpMatrix = Multiply(anchorLineWorldMatrix, Multiply(worldViewMatrix, projectionMatrix));
+		Matrix4x4 sphereLineWvpMatrix = Multiply(sphereLineWorldMatrix, Multiply(worldViewMatrix, projectionMatrix));
+
+
+		if (isMove) {
+
+			pendulum.angularAcceleration = -(9.8f / pendulum.length) * std::sin(pendulum.angle);
+			pendulum.angularVelocity += pendulum.angularAcceleration * deltaTime;
+			pendulum.angle += pendulum.angularVelocity * deltaTime;
+
+			sphere.center.x = pendulum.anchor.x + std::sin(pendulum.angle) * pendulum.length;
+			sphere.center.y = pendulum.anchor.y - std::cos(pendulum.angle) * pendulum.length;
+			sphere.center.z = pendulum.anchor.z;
+		}
+
+		ImGui::Checkbox("IsMove", &isMove);
+		
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -37,6 +95,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		DrawSphere(sphere, worldViewProjectionMatrix, worldViewportMatrix, 0xFFFFFFFF);
+		DrawGrid(worldViewProjectionMatrix, worldViewportMatrix);
+
+		DrawLine(sphere.center, pendulum.anchor, anchorLineWvpMatrix, sphereLineWvpMatrix, worldViewportMatrix, 0xFFFFFFFF);
 
 		///
 		/// ↑描画処理ここまで
